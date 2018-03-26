b82fb147f0175405b8536a6aa2d2037b7bf6c453
SyaifulAndiIrawan <32381985+syaifulandiirawan@users.noreply.github.com>
2018-03-26 18:36:54 +0800

    Instace Object

--- src/praktek1/Alat_Musik_GitarAksi.java
+++ src/praktek1/Alat_Musik_GitarAksi.java
@@ -11,7 +11,9 @@ package praktek1;
  */
 public class Alat_Musik_GitarAksi {
     public static void main(String[] args) {
-        
+       Alat_Musik_Gitar  Akustik = new Alat_Musik_Gitar();
+       Alat_Musik_Gitar  Elekstrik = new Alat_Musik_Gitar();
+       Alat_Musik_Gitar  Bass = new Alat_Musik_Gitar();
     }
     
 }
